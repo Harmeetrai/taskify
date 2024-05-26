5aeba8e40d8917ba1f1493d813883dac3947ca53
Harmeetrai <harmeetrai18@gmail.com>
2024-05-26 10:53:10 -0700

    add convex and clerk authentication and signup

--- src/app/(landing)/_components/heading.tsx
+++ src/app/(landing)/_components/heading.tsx
@@ -3,17 +3,43 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import { Heroes } from "./heroes";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import { SignInButton } from "@clerk/clerk-react";
+
+import Link from "next/link";
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
         Organize your work and life. Welcome to <span>Taskify</span>
       </h1>
-      <h3 className="text-base sm:text-xl md:text-2xl font-medium">Become focused, organized, and calm with taskify.</h3>
-      <Button>
-        Enter Taskify 
-        <ArrowRight className="h-4 w-4 ml-2"/>
-      </Button>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Become focused, organized, and calm with taskify.
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Taskify
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Taskify Free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
+
       <Heroes />
     </div>
   );
--- src/app/(landing)/_components/navbar.tsx
+++ src/app/(landing)/_components/navbar.tsx
@@ -5,9 +5,14 @@ import { useScrollTop } from "../../../../hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Spinner } from "@/components/spinner";
 export const Navbar = () => {
     const scrolled = useScrollTop();
-
+    const { isAuthenticated, isLoading } = useConvexAuth();
     return (
         <div className={cn(
             "z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6",
@@ -15,7 +20,29 @@ export const Navbar = () => {
         )}>
             <Logo/>
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-                <ModeToggle />
+            {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button size="sm">Get Taskify free</Button>
+            </SignUpButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Taskify</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+
+        <ModeToggle />
             </div>
         </div>
 
